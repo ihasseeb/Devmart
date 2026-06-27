b59a184e5b8df7b1afcc11b7d07a595185e25694
HaseeB <117632673+ihasseeb@users.noreply.github.com>
2026-06-27 04:29:51 -0700

    Directory changes

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white"> DevMart is Live! 🚀</h1>
+    </div>
+  )
+}
+export default App
